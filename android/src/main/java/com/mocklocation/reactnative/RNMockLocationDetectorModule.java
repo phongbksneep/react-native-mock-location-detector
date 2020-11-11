0bb9b67b0aa6885cb543e95e8cbdbb3b138e62a6
phongbksneep <48904562+phongbksneep@users.noreply.github.com>
2020-11-11 09:44:59 +0700

    Update RNMockLocationDetectorModule.java
    return value

--- android/src/main/java/com/mocklocation/reactnative/RNMockLocationDetectorModule.java
+++ android/src/main/java/com/mocklocation/reactnative/RNMockLocationDetectorModule.java
@@ -3,6 +3,7 @@ package com.mocklocation.reactnative;
 
 import android.widget.Toast;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -22,14 +23,14 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -40,7 +41,7 @@ import java.util.List;
 public class RNMockLocationDetectorModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
-
+  public static Promise promise;
   public RNMockLocationDetectorModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
@@ -55,11 +56,13 @@ public class RNMockLocationDetectorModule extends ReactContextBaseJavaModule {
   /** Java code for checkLocationProvide */
   @ReactMethod
   public void checkMockLocationProvider(final String dialogTitle, final String dialogMessage,
-      final String dialogButtonText) {
+      final String dialogButtonText, Promise promise) {
+      this.promise = promise;
     if (ActivityCompat.checkSelfPermission(getCurrentActivity(),
         Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
         && ActivityCompat.checkSelfPermission(getCurrentActivity(),
             Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      promise.resolve(false);
       return;
     }
     FusedLocationProviderClient mFusedLocationClient;
@@ -78,16 +81,21 @@ public class RNMockLocationDetectorModule extends ReactContextBaseJavaModule {
                 builder.setCancelable(false);
                 builder.setPositiveButton(dialogButtonText, new DialogInterface.OnClickListener() {
                   public void onClick(DialogInterface dialog, int which) {
-                    getCurrentActivity().finish();
+                    // getCurrentActivity().finish();
                   }
                 });
                 builder.show();
-
-              } 
+                RNMockLocationDetectorModule.promise.resolve(true);
+              } else {
+                RNMockLocationDetectorModule.promise.resolve(false);
+              }
+            } else {
+              RNMockLocationDetectorModule.promise.resolve(false);
             }
           }
 
         });
+    return;
   }
 
   public boolean isLocationFromMockProvider(Context context, Location location) {
